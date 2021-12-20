160f230028d1648b13bed12e91fac557fc036342
MattiaM99 <mattiamazzoni123@gmail.com>
2021-12-20 15:22:29 +0100

    fine snack 5

---
+++ snack-5/index.php
@@ -0,0 +1,18 @@
+<?php
+$paragrafo = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolorem ducimus iste vero tempora nesciunt tenetur atque, obcaecati eaque sunt corporis optio, ad corrupti neque quia et quis repudiandae facilis culpa nam quam veritatis, non hic. Ducimus praesentium accusamus itaque neque aliquid esse facilis dolorem. Asperiores dolor saepe voluptatum debitis!";
+
+$words = var_dump(explode(".", $paragrafo));
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
